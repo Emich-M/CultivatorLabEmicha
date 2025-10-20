285b0b9be7d41ca082a9b74ad592412e6d71633e
Emich-M <fagetolrer@gmail.com>
2025-10-20 23:18:53 +0300

    Zadanie

--- main.cpp
+++ main.cpp
@@ -1,6 +1,76 @@
 #include <iostream>
+int** create(size_t rows, size_t cols);
+void remove(int** m, size_t rows, size_t cols);
+void input(int** m, size_t rows, size_t cols);
+void output(const int* const* m, size_t rows, size_t cols);
+int** convert(const int* t, size_t n, const size_t* lns, size_t rows);
+
 int main()
 {
-	std::cout << "Hello, Vasiliy Kasilov!!!";
-	std::cout << "\n";
+    size_t rows = 0, cols = 0;
+    std::cin >> rows >> cols;
+    if (!std::cin) {
+        return 1;
+    }
+
+    int** m = create(rows, cols);
+    input(m, rows, cols);
+    output(m, rows, cols);
+    remove(m, rows, cols);
+    return 0;
+}
+
+void input(int** m, size_t rows, size_t cols)
+{
+    for (size_t i = 0; i < rows && std::cin; ++i) {
+        for (size_t j = 0; j < cols && std::cin; ++j) {
+            std::cin >> m[i][j];
+        }
+    }
+}
+
+void output(const int* const* m, size_t rows, size_t cols) {
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < cols; ++j) {
+            std::cout << m[i][j];
+            if (j + 1 < cols) std::cout << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+int** create(size_t rows, size_t cols) {
+    int** result = new int*[rows];
+    size_t i=0;
+    try {
+        for (; i < rows; ++i) {
+            result[i] = new int[cols];
+        }
+    }
+    catch (...) {
+        remove(result, i, cols);
+        throw;
+    }
+    return result;
+}
+
+void remove(int** m, size_t rows, size_t /*cols*/) {
+    for (size_t i = 0; i < rows; ++i) {
+        delete[] m[i];
+    }
+    delete[] m;
+}
+
+int** convert(const int* t, size_t n, const size_t* lns, size_t rows){
+    int** res = new int*[rows];
+    const int* position = t;
+    for (size_t i =0; i < rows; ++i){
+        size_t len = lns[i];
+        res[i] = new int[len];
+        for (size_t j=0; j<len; ++j){
+            res[i][j] = *position;
+            ++position;
+        }
+    }
+    return res;
 }
